5d7e5b073dc88f29e899091969636b4d9f939933
Sarah <Ms.sarahelizabethb@gmail.com>
2018-05-28 17:39:59 -0400

    first commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Views.Home;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SignUpForm()
+        {
+            return View();
+        }
+        public ActionResult AddNewUser(UserInfo newUser)
+        {
+            // Validation
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Welcome{newUser.Username}";
+                return View("Confirm");
+            }
+            else
+            {
+                ViewBag.Address = Request.UserHostAddress;
+                return View("Error");
+            }
+            // to insert newUser intot the DB
+
+
+        }
+
+
+    }
+}       
+        
---
+++ Lab21/Views/Home/UserInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Views.Home
+{
+    public class UserInfo
+    {
+        [Required]
+            public string First { get; set; }
+        [Required]
+            public string Last { get; set; }
+        [Required]
+            public string Username { get; set; }
+        [Required]
+            public string Password { get; set; }
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$
+        ", ErrorMessage = "Bad Email!")]
+        public string Email { get; set; }
+            public string Address { get; set; }
+            public string State { get; set; }
+            public DateTime Bday { get; set; }
+            public string Male { get; set; }
+            public string Female { get; set; }
+            public string Other { get; set; }
+            
+            public UserInfo()
+            {
+            }
+            
+        public UserInfo(string first, string last, string username, string password, 
+            string email, string address, string state, DateTime bday, string male, string female, string other)
+        {
+            First = first;
+            Last = last;
+            Username = username;
+            Password = password;
+            Email = email;
+            Address = address;
+            State = state;
+            Bday = bday;
+            Male = male;
+            Female = female;
+            Other = other;
+
+        }
+    }
+}
